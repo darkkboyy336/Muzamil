de58a8b728393c4ae2bab1d92d0f08aa2ef8e7b6
darkkboyy336 <darkkboyy336@gmail.com>
2026-01-05 15:39:34 +0530

    Create config.env.js

---
+++ config.env.js
@@ -0,0 +1,23 @@
+// config.env.js
+window.PORTFOLIO_CONFIG = {
+    ADMIN_PIN: "3366",  // Set your PIN here
+    
+    SECURITY: {
+        MAX_ATTEMPTS: 3,
+        LOCKOUT_TIME: 300000,
+        SESSION_TIME: 1800000
+    },
+    
+    CONTACT: {
+        PHONE: "+917889737018",
+        EMAIL: "Muzamilmajeed23@Outlook.com",
+        LINKEDIN: "https://www.linkedin.com/in/muzamil-majeed-9b00361b2",
+        WHATSAPP: "https://wa.me/+917889737018"
+    },
+    
+    INITIAL_DATA: {
+        profileName: "Muzamil Majeed",
+        profileBio: "MBA Professional | MSR at Brakes India Pvt Limited",
+        profileImage: "https://i.ibb.co/ZRbTbg3B/IMG-20251221-WA0012.jpg"
+    }
+};
